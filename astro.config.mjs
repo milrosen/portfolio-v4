d27fb51d36e1cf71c587d64800891d0b53996fd5
milton <milton.rosenbaum@gmail.com>
2024-10-15 20:22:43 -0400

    half of the first post, adding math

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 import react from '@astrojs/react'
 import mdx from '@astrojs/mdx'
+
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://milrosen.github.io',
   base: 'portfolio-v4',
+  markdown: {
+    syntaxHighlight: "prism",
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
   integrations: [react(), mdx()]
 });
